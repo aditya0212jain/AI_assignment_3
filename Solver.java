5b6399fff56808dac20aa6a9dd253c871ea1acd3
aditya0212jain <aditya0212jain@gmail.com>
2018-09-27 01:27:13 -0700

    added the completeness of graph clauses

--- Solver.java
+++ Solver.java
@@ -11,6 +11,23 @@ public class Solver {
 	public static int k;
 	public static boolean[][] isEdge;
 	public static boolean[][] inSub;
+
+	static int getI(int b){
+		return (b-1)/vertices;
+	}
+
+	static int getV(int b){
+		if(b%vertices==0){
+			return vertices;
+		}else{
+			return b%vertices;
+		}
+	}
+
+	static int getID(int i,int v){
+		return (i*vertices+v);
+	}
+
 	public static void main(String[] args) throws IOException{
 		FileInputStream f = new FileInputStream(args[0]+".graph");
 		Scanner s = new Scanner(f);
@@ -35,7 +52,23 @@ public class Solver {
 		s.close();
 		FileWriter file = new FileWriter(args[0]+".satinput");
 		BufferedWriter bf = new BufferedWriter(file);
-		bf.write("p cnf "+(k*vertices)+" "+"5");
+		bf.write("p cnf "+(k*vertices)+" "+"5\n");
+		//-----------------------------Clauses for the Completeness of graphs---------------------------------
+		for(int i=0;i<vertices;i++){
+			for(int j=i;j<vertices;j++){
+				if(isEdge[i][j]==false){
+					for(int l=0;l<k;l++){
+						int firstVar = getID(l,i+1);
+						int secondVar = getID(l,j+1);
+						firstVar = -1*firstVar;
+						secondVar = -1*secondVar;
+						bf.write(firstVar+" "+secondVar+" 0"+"\n");
+					}
+				}
+			}
+		}
+
+
 		bf.close();
 		file.close();
 	}
